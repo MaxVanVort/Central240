a96bcf0cee88cd05fa89ae3de574359482f86f1f
SSsM001 <118392057+SSsM001@users.noreply.github.com>
2023-04-09 12:22:05 -0400

    the new game class

---
+++ NewGame.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class NewGame {
+
+    public JButton b1 = new JButton("");
+    public JButton b2 = new JButton("");
+    public JButton b3 = new JButton("");
+    public JButton b4 = new JButton("");
+    public JButton b5 = new JButton("");
+    public JButton b6 = new JButton("");
+    public JButton b7 = new JButton("");
+    public JButton b8 = new JButton("");
+    public JButton b9 = new JButton("");
+    public String wLT = "w/l/t";
+    public JButton Menu = new JButton("MENU");
+    private boolean turn;
+    private boolean GameOver;
+    private JPanel grid;
+    private GridLayout layout;
+    private BoxLayout mainLayout;
+
+
+}
