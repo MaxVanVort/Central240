cfddf7a2ad1cc9bc10cdd84f113915caf669c63b
George-Yergey <gyergey@mail.umw.edu>
2023-04-15 12:35:05 -0400

    Commit updated Menu

--- Menu.java
+++ Menu.java
@@ -9,6 +9,12 @@ public class Menu {
     public static JButton sbButton = new JButton("scorboard");
     public static JButton exitButton = new JButton("exit");
     public Menu(){
+        frame = new JFrame("menu");
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.setLayout(new BorderLayout(20,15));
+        frame.add(newGameButton, BorderLayout.CENTER);
+        frame.add(sbButton, BorderLayout.CENTER);
+        frame.add(exitButton, BorderLayout.CENTER);
 
     }
 
@@ -25,7 +31,9 @@ class ButtonListener implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(button.getText().equals ("new game")){
             NewGame ng = new NewGame(frame);
-        }else if(){
+        }else if(button.getText().equals ("exit")){
+            System.exit (0);
+        }{
 
         }
     }
